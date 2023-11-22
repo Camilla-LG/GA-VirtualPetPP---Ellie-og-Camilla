d784e632811d56fc70d6b6371beaf352498594bd
Ellie Marie <orix@hotmail.no>
2023-11-22 14:16:13 +0100

    Add project files.

---
+++ VirtualPetPP/Pet.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualPetPP
+{
+    internal class Pet
+    {
+        public string Name { get; set; }
+        public string Animal { get; set; }
+        public int HungerCount { get; set; }
+        public int Boredom { get; set; }
+        public int Snuggle { get; set; }
+        public bool PoopTime { get; set; }
+
+        public Pet(
+            string name,
+            string animal,
+            int hungerCount,
+            int boredom,
+            int snuggle,
+            bool poopTime
+            )
+        {
+            Name = name;
+            Animal = animal;
+            HungerCount = hungerCount;
+            Boredom = boredom;
+            Snuggle = snuggle;
+            PoopTime = poopTime;
+        }
+    }
+}
---
+++ VirtualPetPP/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace VirtualPetPP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Welcome to our pet world!");
+            Console.WriteLine("--------------------------------");
+
+            var turbo = new Pet("Turbo", "Cat", 0, 0, 0, false);
+            var marve = new Pet("Marve", "Dog", 0, 0, 0, false);
+
+            while (true)
+            {
+                Console.WriteLine("Choose your pet:");
+                Console.WriteLine("1. Turbo");
+                Console.WriteLine("2. Marve");
+
+                char petChoice = Console.ReadKey().KeyChar;
+
+                switch (petChoice)
+                {
+                    case '1':
+                        Console.WriteLine("You picked Turbo!");
+                        InteractWithPet(turbo);
+                        break;
+                    case '2':
+                        Console.WriteLine("You picked Marve!");
+                        InteractWithPet(marve);
+                        break;
+                }
+            }
+        }
+
+        static void InteractWithPet(Pet pet)
+        {
+            while (true)
+            {
+                Console.WriteLine("Choose an option:");
+                Console.WriteLine("1. Feed");
+                Console.WriteLine("2. Play");
+                Console.WriteLine("3. Snuggle");
+                Console.WriteLine("4. Check if pet needs to poop");
+                Console.WriteLine();
+                Console.WriteLine("5. Exit pet world");
+
+                char choice = Console.ReadKey().KeyChar;
+
+                switch (choice)
+                {
+                    case '1':
+                        Console.WriteLine($"{pet.Name} is happy, rolling and fed.");
+                        break;
+                    case '2':
+                        Console.WriteLine("Wohooo, playtime!");
+                        break;
+                    case '3':
+                        Console.WriteLine("Purr purr, that's good!");
+                        // Kan legge inn en if for høyre eller venstre
+                        break;
+                    case '4':
+                        Console.WriteLine("Poopie");
+                        pet.PoopTime = PoopieTime(pet.PoopTime);
+                        break;
+                    case '5':
+                        Console.WriteLine("Pet world is shutting down. Byebye!");
+                        return;
+                }
+            }
+        }
+
+        public static bool PoopieTime(bool currentPoopieTime)
+        {
+            return !currentPoopieTime;
+        }
+
+        // Legge til "PassTime()" metode med random funksjonalitet.
+    }
+}
